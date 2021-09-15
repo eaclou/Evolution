bf4713088e9ebdf7cc9829fc7838ebc16a3c3758
Will Petillo <will.petillo@gmail.com>
2021-09-14 17:47:10 -0700

    Removed DrawIf plugin
    Wasn't working as expected, causing build errors.

--- Assets/Draw If Atrribute/DisablingType.cs
+++
@@ -1,8 +0,0 @@
-﻿/// <summary>
-/// The ways you can disable a field when using the DrawIf attribute and the condition isn't met.
-/// </summary>
-public enum DisablingType
-{
-    ReadOnly = 2,
-    DontDraw = 3
-}
