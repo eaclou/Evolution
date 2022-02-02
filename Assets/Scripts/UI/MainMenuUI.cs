0e718e9d993b4d50884c8c2828ea94794bc7d867
eaclou <eaclou@gmail.com>
2022-02-02 12:33:08 -0500

    Prepping for MAin MEnu Animation

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -7,7 +7,7 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] Button buttonNewSimulation;
     //[SerializeField] Text textMouseOverInfo;
     //[SerializeField] Text quickStartText;
-    [SerializeField] GameObject panelGameOptions;
+    //[SerializeField] GameObject panelGameOptions;
     
     GameManager gameManager => GameManager.instance;
     Profile profile => gameManager.activeProfile;
@@ -37,7 +37,7 @@ public class MainMenuUI : MonoBehaviour
         // *** For now, 1 sim at a time ***
         if (!firstTimeStartup) 
         {
-            buttonNewSimulation.gameObject.SetActive(false); 
+            //buttonNewSimulation.gameObject.SetActive(false); 
             //textMouseOverInfo.gameObject.SetActive(false);
         }
         
@@ -54,26 +54,26 @@ public class MainMenuUI : MonoBehaviour
     {
         optionsMenuOn = false;
         controlsMenuOn = false;
-        Refresh();        
+        //Refresh();        
     }
     
     public void ClickControlsMenu()
     {
         controlsMenuOn = !controlsMenuOn;
         optionsMenuOn = false;
-        Refresh();
+        //Refresh();
     }
     
     public void ClickOptionsMenu() 
     {
         optionsMenuOn = !optionsMenuOn;
         controlsMenuOn = false;
-        Refresh();
+        //Refresh();
     }
     
     public void ClickQuickStart() { StartGame(true); }
     
-    public void ClickNewSimulation() { StartGame(false); }
+    public void ClickNewSimulation() { /*StartGame(false);*/ }
     
     void StartGame(bool isQuickStart)
     {
@@ -86,42 +86,5 @@ public class MainMenuUI : MonoBehaviour
             gameManager.ResumePlaying();
         }          
     }
-    /*
-    public void MouseEnterQuickStart() {
-        //textMouseOverInfo.gameObject.SetActive(true);
-        //textMouseOverInfo.text = "Start with an existing ecosystem full of various living organisms.";
-    }
     
-    public void MouseExitQuickStart() {
-        textMouseOverInfo.gameObject.SetActive(false);
-    }
-
-    public void MouseEnterNewSimulation() {
-        //textMouseOverInfo.gameObject.SetActive(true);
-        //textMouseOverInfo.text = "Create a brand new ecosystem from scratch. It might take a significant amount of time for intelligent creatures to evolve.\n*Not recommended for first-time players.";
-    }
-    
-    public void MouseExitNewSimulation() {
-        textMouseOverInfo.gameObject.SetActive(false);
-    }
-
-    public void MouseEnterControlsButton() {
-        if (optionsMenuOn) return;
-        textMouseOverInfo.gameObject.SetActive(true);
-        textMouseOverInfo.text = "Arrows or WASD for movement, scrollwheel for zoom. 'R' and 'F' tilt Camera.\nKeyboard & Mouse only - Controller support coming soon.";
-    }
-    
-    public void MouseExitControlsButton() {
-        textMouseOverInfo.gameObject.SetActive(false);
-    }
-    */
-    public void Refresh() 
-    {
-        panelGameOptions.SetActive(optionsMenuOn);
-    
-        if (optionsMenuOn) 
-        {
-            //textMouseOverInfo.gameObject.SetActive(false);
-        }
-    }
 }
