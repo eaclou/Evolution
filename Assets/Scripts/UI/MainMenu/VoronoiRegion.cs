85ff229a492d40784e411691c501ff9d5cbe72b0
eaclou <eaclou@users.noreply.github.com>
2024-05-09 20:01:48 -0400

    voronoi added but not liked

---
+++ Assets/Scripts/UI/MainMenu/VoronoiRegion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoronoiRegion : MonoBehaviour
+{
+    public GameObject GO;
+    public Color color;
+    public int ID;
+    public string name;
+    public Vector3 rootCoords01;  // resolution-independent
+    public Vector3 rootPixelPos; // resolution-specific
+    
+}
--- Assets/Scripts/UI/MainMenuBackgroundGenerator.cs
+++ Assets/Scripts/UI/MainMenuBackgroundGenerator.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuBackgroundGenerator : MonoBehaviour
 {
-    [System.Serializable]
-    public struct VoronoiRegion
-    {
-        public int ID;
-        public string name;
-        public Vector3 rootCoords01;  // resolution-independent
-        public Vector3 rootPixelPos; // resolution-specific
-    }
+    [SerializeField] ComputeShader computeShaderMainMenuBG; 
+    [SerializeField] Image imageRenderTexDisplay;
+    [SerializeField] Material displayMat;
+        
     [SerializeField]
     public List<VoronoiRegion> voronoiRegionsList;
+    private Vector2[] voronoiArray;
+    private ComputeBuffer voronoiComputeBuffer;
     //[ReadOnly]
     //public Vector3 res;
 
@@ -23,6 +22,13 @@ public class MainMenuBackgroundGenerator : MonoBehaviour
     // Regions should instead be ScriptableObjects/MonoBehaviors and Set within UI panel?
     //shader may have to be hardcoded for the Region-specific graphics?
     //animate transion Lerp variables and pipe that into gpuShader for animated transitions
+    private RenderTexture sourceRT;
+    private RenderTexture targetRT;
+    private float mousePosX01;
+    private float mousePosY01;
+    private bool isInit = false;
+    private int defaultResolutionX = 1920;
+    private int defaultResolutionY = 1080;
     
     // Start is called before the first frame update
     void Start()
@@ -34,12 +40,103 @@ public class MainMenuBackgroundGenerator : MonoBehaviour
             Debug.LogError("no voronoi regions list!!");
         }
         RefreshRegionsCenterPixelPos();
+
+        Initialize(); // shader stuffs
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void FixedUpdate() {
+        if (isInit) {            
+
+            TickComputeShader();
+        }  
+    }
+
+    public void Initialize() {
+
+        defaultResolutionX = Screen.currentResolution.width;
+        defaultResolutionY = Screen.currentResolution.height;
+
+        if (sourceRT != null) {
+            sourceRT.Release();
+        }
+        sourceRT = new RenderTexture(defaultResolutionX, defaultResolutionY, 1, RenderTextureFormat.ARGBFloat);
+        sourceRT.enableRandomWrite = true;
+        sourceRT.filterMode = FilterMode.Point;
+        sourceRT.wrapMode = TextureWrapMode.Repeat;
+        sourceRT.Create();   
         
+        if (targetRT != null) {
+            targetRT.Release();
+        }
+        targetRT = new RenderTexture(defaultResolutionX, defaultResolutionY, 1, RenderTextureFormat.ARGBFloat);
+        
+        targetRT.enableRandomWrite = true;
+        targetRT.filterMode = FilterMode.Point;
+        targetRT.wrapMode = TextureWrapMode.Repeat;
+        targetRT.Create();
+
+        displayMat.SetTexture("_MainTex", sourceRT);
+             
+        UpdateComputeShaderParams();
+        int kernelCSInit = computeShaderMainMenuBG.FindKernel("CSInit");
+        computeShaderMainMenuBG.SetTexture(kernelCSInit, "SourceRT", sourceRT);
+        computeShaderMainMenuBG.SetTexture(kernelCSInit, "TargetRT", targetRT);
+        computeShaderMainMenuBG.Dispatch(kernelCSInit, defaultResolutionX / 32, defaultResolutionY / 32, 1);
+
+        kernelCSInit = computeShaderMainMenuBG.FindKernel("CSInit");
+        UpdateComputeShaderParams();
+        computeShaderMainMenuBG.SetTexture(kernelCSInit, "SourceRT", targetRT);
+        computeShaderMainMenuBG.SetTexture(kernelCSInit, "TargetRT", sourceRT);
+        computeShaderMainMenuBG.Dispatch(kernelCSInit, defaultResolutionX / 32, defaultResolutionY / 32, 1);
+
+
+        isInit = true;
+    }
+    private void UpdateComputeShaderParams() {
+                
+        // Parameters:        
+        computeShaderMainMenuBG.SetFloat("_ResolutionX", defaultResolutionX);    
+        computeShaderMainMenuBG.SetFloat("_ResolutionY", defaultResolutionY);   
+        computeShaderMainMenuBG.SetFloat("_MouseCoordX", mousePosX01);
+        computeShaderMainMenuBG.SetFloat("_MouseCoordY", mousePosY01);        
+        computeShaderMainMenuBG.SetFloat("_Time", Time.realtimeSinceStartup);
+    }
+
+    public void TickComputeShader() {
+        
+        voronoiArray = new Vector2[8];
+        for(int i = 0; i < voronoiArray.Length; i++) {
+            voronoiArray[i] = new Vector2(0f, 0f);
+            if (i < voronoiRegionsList.Count) {
+                //Vector3 GameObjectPosition = voronoiRegionsList[i].GO.transform.position;
+                //RectTransform rect = voronoiRegionsList[i].GetComponent<RectTransform>();
+                //var centerPoint= rectTransform.TransformPoint(rectTransform.rect.center);
+                Vector3 GameObjectPosition = voronoiRegionsList[i].GO.GetComponent<RectTransform>().position;
+                voronoiRegionsList[i].rootPixelPos = GameObjectPosition;
+                //rect.anchoredPosition;// new Vector3(i * 40, 500, 0);// Camera.main.WorldToScreenPoint(rect.position);
+                voronoiArray[i] = new Vector2(GameObjectPosition.x, GameObjectPosition.y);
+            }
+            
+        }
+        if(voronoiComputeBuffer != null) {
+            voronoiComputeBuffer.Dispose();
+        }
+        voronoiComputeBuffer = new ComputeBuffer(voronoiArray.Length, sizeof(float) * 2);
+        voronoiComputeBuffer.SetData(voronoiArray);
+        //tick
+        UpdateComputeShaderParams();
+        int kernelCSTick = computeShaderMainMenuBG.FindKernel("CSTick");
+        computeShaderMainMenuBG.SetBuffer(kernelCSTick, "VoronoiBuffer", voronoiComputeBuffer);
+        computeShaderMainMenuBG.SetTexture(kernelCSTick, "SourceRT", sourceRT);
+        computeShaderMainMenuBG.SetTexture(kernelCSTick, "TargetRT", targetRT);        
+        computeShaderMainMenuBG.Dispatch(kernelCSTick, defaultResolutionX / 32, defaultResolutionY / 32, 1);
+
+        //.tock
+        kernelCSTick = computeShaderMainMenuBG.FindKernel("CSTick");   
+        computeShaderMainMenuBG.SetBuffer(kernelCSTick, "VoronoiBuffer", voronoiComputeBuffer);
+        computeShaderMainMenuBG.SetTexture(kernelCSTick, "SourceRT", targetRT); // !!! SWAP TEXTURES --> send back to orig
+        computeShaderMainMenuBG.SetTexture(kernelCSTick, "TargetRT", sourceRT);        
+        computeShaderMainMenuBG.Dispatch(kernelCSTick, defaultResolutionX / 32, defaultResolutionY / 32, 1);
     }
 
     void RefreshRegionsCenterPixelPos() {
@@ -69,5 +166,11 @@ public class MainMenuBackgroundGenerator : MonoBehaviour
         }
 
         return closestID;
+    }
+
+    private void OnDisable() {
+        if (voronoiComputeBuffer != null) {
+            voronoiComputeBuffer.Dispose();
+        }
     }
 }
